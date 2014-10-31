0fa6348ddb38a8addef646366febff6ed82b9deb
Chinfeng Chung <chinfeng.chung@gmail.com>
2014-10-31 19:32:52 +0800

    20141031r

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 __author__ = 'chinfeng'
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='gumpy',
-    version='0.1',
+    version='0.2',
     description='An dynamic component framework',
     author='Chinfeng Chung',
     author_email='chinfeng.chung@gmail.com',
-    packages=find_packages(exclude=['tests']),
     classifiers=['Programming Language :: Python :: 2.7'],
     classifiers=['Programming Language :: Python :: 3.4'],
     test_suite = 'tests',
